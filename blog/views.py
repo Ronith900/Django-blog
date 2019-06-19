eca76785204ca63db63ab1761928c26ef8c1abb4
Ronith <ronith900@gmail.com>
2019-06-20 07:33:37 +0800

    Intial commit

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import post
+from django.views.generic import (
+	ListView,
+    DetailView,
+    CreateView
+)
+
+
+def home(request):
+    context = {
+        'posts':post.objects.all(),
+        'title': "Home"
+    }
+    return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+    model = post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = post
+
+def about(request):
+    return render(request,'blog/about.html',{'title':"About"})
+
+class PostCreateView(CreateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
